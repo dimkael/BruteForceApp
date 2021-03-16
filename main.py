0409d400c51937b0390a7b26c2d03eae8bc574aa
dimkael <53169743+dimkael@users.noreply.github.com>
2021-03-17 02:36:23 +0300

    added simple server

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 from pass_gens import bad_pass_gen
 
 
+if __name__ == '__main__':
+    login = 'admin'
+    success = False
+    while not success:
+        password = next(bad_pass_gen)
+        data = {'login': login, 'password': password}
 
-login = 'admin'
-success = False
-while not success:
-    password = next(bad_pass_gen)
-    data = {'login': login, 'password': password}
-
-    response = requests.get('http://google.com/')
+        response = requests.get('http://google.com/')
